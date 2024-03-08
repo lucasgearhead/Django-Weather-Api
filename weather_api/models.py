8068593f14ea35d746f0ec533d651ae2de1f94ed
lucasdcsilva04 <jeffersond1silva@hotmail.com>
2024-03-08 17:06:45 -0300

    feat: created models.py

--- weather_api/models.py
+++ weather_api/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Weather(models.Model):
+    city = models.CharField(max_length=100)
+    temperature = models.FloatField()
+    description = models.CharField(max_length=200)
