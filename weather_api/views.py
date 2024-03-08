b252a693b55cd6914a6b8601485c1fe5ba9db00b
lucasdcsilva04 <jeffersond1silva@hotmail.com>
2024-03-08 17:17:01 -0300

    fix: get all json of external api

--- weather_api/views.py
+++ weather_api/views.py
@@ -1,25 +1,21 @@
-from django.shortcuts import render
-
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from serializers import WeatherSerializer
 from .models import Weather
+from serializers import WeatherSerializer
 import requests
 
 class WeatherAPIView(APIView):
     def get(self, request):
         city = request.query_params.get('city', 'Sorocaba')
-        url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid=1871496f2ca4459545804dd8e8be545b"
+        url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid=YOUR_API_KEY"
         response = requests.get(url)
         data = response.json()
 
-        # Save data in the database
+        # Salvando o JSON completo no banco de dados
         weather_data = Weather.objects.create(
-            city=data['name'],
-            temperature=data['main']['temp'],
-            description=data['weather'][0]['description']
+            city=city,
+            json_data=data  # Salvando o JSON completo
         )
 
         serializer = WeatherSerializer(weather_data)
         return Response(serializer.data)
-
