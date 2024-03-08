f6243b79807812bd69f6ce543a645d2dea11acb7
lucasdcsilva04 <jeffersond1silva@hotmail.com>
2024-03-08 17:18:57 -0300

    fix: import of serializer

--- weather_api/views.py
+++ weather_api/views.py
@@ -1,7 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Weather
-from serializers import WeatherSerializer
+from weather_api.serializers import WeatherSerializer
 import requests
 
 class WeatherAPIView(APIView):
