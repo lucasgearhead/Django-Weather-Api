f181cd5fdda76196e29b06878a52fcd66738bdce
lucasdcsilva04 <jeffersond1silva@hotmail.com>
2024-03-11 17:45:54 -0300

    feat(urls): Adicionar rotas para novas views de histórico de clima

--- weather_project/urls.py
+++ weather_project/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from weather_api.views import WeatherAPIView
+from weather_api.views import WeatherAPIView, WeatherHistoryDetailAPIView, WeatherHistoryListAPIView 
 
 urlpatterns = [
-    path('weather/', WeatherAPIView.as_view()),
+    path('weather/', WeatherAPIView.as_view(), name='weather'),
+    path('history/', WeatherHistoryListAPIView.as_view(), name='weather-history-list'),
+    path('history/<int:pk>/', WeatherHistoryDetailAPIView.as_view(), name='weather-history-detail'),
 ]
+
