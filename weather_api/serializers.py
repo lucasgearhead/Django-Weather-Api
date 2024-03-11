f662262180a816b2bfbadb55776716f0cb1c1b64
lucasdcsilva04 <jeffersond1silva@hotmail.com>
2024-03-11 17:43:13 -0300

    feat(serializers): Atualizar serializer para incluir novas colunas do modelo Weather

--- weather_api/serializers.py
+++ weather_api/serializers.py
@@ -4,4 +4,4 @@ from .models import Weather
 class WeatherSerializer(serializers.ModelSerializer):
     class Meta:
         model = Weather
-        fields = '__all__'
+        fields = ['city', 'coord_lon', 'coord_lat', 'weather_main', 'weather_description', 'temp', 'feels_like', 'temp_min', 'temp_max', 'pressure', 'humidity', 'sea_level', 'grnd_level', 'visibility', 'wind_speed', 'wind_deg', 'wind_gust', 'rain_one_hr', 'rain_three_hr', 'snow_one_hr', 'snow_three_hr', 'clouds_all', 'date_time', 'country', 'sunrise', 'sunset', 'timezone', 'cod']
